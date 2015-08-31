7da4db0bb63c5f0a530caeab00f5c970cf9ce1a6
Andrew Bryant <andrewpb@vt.edu>
2015-08-31 17:11:43 -0400

    Now logs ToDo list to correct file, whether default or user inputed.

--- todo.cpp
+++ todo.cpp
@@ -4,6 +4,7 @@
 void todo(istream& in, ostream& out)
 {
 	ToDoStruct *toDo; //initialize the array type
+	ToDoStruct *doneToDo;//"	"
 
 	int size = 10; //Initial size is 10
 
@@ -12,14 +13,45 @@ void todo(istream& in, ostream& out)
 	ofstream myLogging;
 
 	toDo = new ToDoStruct[size]; //Array of initial size 10 made of of toDoStruct defined in header
+	doneToDo = new ToDoStruct[size]; //Todostruct array of size 10 of completed tasks 
 
-	string command; //string variable for command to be read into 
+	string start; //string variable for command to be read into 
 
-	in >> command; //sets first word from user input as the command before entering while loop
+	in >> start; //sets first word from user input as the command before entering while loop
 
 	//continue while input does not fail 
 	while (!in.fail())
 	{
+		//if the first word in the command is not todo, stop the program
+		if (start != "todo")
+			break;
 
+		//Call the next word in the input temp until we know if it is a command or a file name 
+		string temp;
+		in >> temp;
+
+		//create filename and command variables 
+		string fileName;
+		string command;
+
+		//if the user specifies their own filename, open that txt file as ofstream, and get command 
+		if (temp == "-f")
+		{
+			in >> fileName;
+			myLogging.open(fileName.c_str());
+			in >> command;
+		}
+
+		//If the user does not specify their own file, use standard file and set command string variable to the contents of the temp string variable 
+		else
+		{
+			fileName = "todo.txt";
+			myLogging.open(fileName.c_str());
+			command = temp;
+		}
+
+
+
+		in >> start; // reprime
 	}
 }
